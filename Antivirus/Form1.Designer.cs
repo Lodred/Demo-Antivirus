a3dc53acbeee465cf6b9e28fb9c9c90247943965
Lodred <lorminu@gmail.com>
2022-11-13 21:23:17 -0300

    Primer Prototipo

---
+++ Antivirus/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Antivirus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.label1.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(200, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(383, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "AVASTO FURII 2202";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(100, 70);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(600, 20);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "No se ha detectado Malware aún."});
+            this.listBox1.Location = new System.Drawing.Point(100, 100);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(600, 199);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 310);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 25);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Seleccionar Carpeta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(227, 310);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 25);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Escanear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(302, 310);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(69, 25);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Remover";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label2.Location = new System.Drawing.Point(102, 338);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Carpeta seleccionada : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Crimson;
+            this.label3.Location = new System.Drawing.Point(102, 353);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Dectados : 0";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(377, 310);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(69, 25);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Reparar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 371);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Avasto Antivirus";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Antivirus/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Antivirus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int viruses = 0;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            label2.Text = "Carpeta seleccionada : " + folderBrowserDialog1.SelectedPath;
+            viruses = 0;
+            progressBar1.Value = 0;
+            listBox1.Items.Clear();
+            button2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            viruses = 0;
+            string[] search = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.*");
+            progressBar1.Maximum = search.Length;
+            foreach (string item in search)
+            {
+                try
+                {
+                    StreamReader stream = new StreamReader(item);
+                    string read = stream.ReadToEnd();
+                    string[] virus = new string[] { "VIRUSES", "INFECTED", "HACKED" };
+                    foreach (string st in virus)
+                    {
+                        if (Regex.IsMatch(read, st))
+                        {
+                            viruses += 1;
+                            listBox1.Items.Add(item);
+                            button3.Show();
+                            button4.Show();
+                            label3.Text = "Dectados : " + viruses.ToString();
+                        }
+                        progressBar1.Increment(1);
+                    }
+                }
+                catch
+                {
+                    string read = item;
+                    string[] virus = new string[] { "VIRUSES", "INFECTED", "HACKED" };
+                    foreach (string st in virus)
+                    {
+                        if (Regex.IsMatch(read, st))
+                        {
+                            viruses += 1;
+                            listBox1.Items.Add(item);
+                            button3.Show();
+                            button4.Show();
+                            label3.Text = "Dectados : " + viruses.ToString();
+
+                        }
+                        progressBar1.Increment(1);
+
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Hide();
+            button3.Hide();
+            button4.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string removex = listBox1.Text;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            if (this.listBox1.Text != ""){
+                File.Delete(this.listBox1.Text);
+                MessageBox.Show("El archivo seleccionado fue removido");
+                listBox1.Items.Clear();
+                button2.PerformClick();
+            }
+            else
+            {
+                MessageBox.Show("Selecciona un archivo");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string removex = listBox1.Text;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            if (this.listBox1.Text != "")
+            {
+                string text = File.ReadAllText(this.listBox1.Text);
+                text = text.Replace("INFECTED", "LIMPIO1");
+                text = text.Replace("VIRUSES", "LIMPIO2");
+                text = text.Replace("HACKED", "LIMPIO3");
+                File.WriteAllText(this.listBox1.Text, text);
+                MessageBox.Show("El archivo seleccionado fue reparado");
+                listBox1.Items.Clear();
+                button2.PerformClick();
+            }
+            else
+            {
+                MessageBox.Show("Selecciona un archivo");
+            }
+        }
+    }
+}
